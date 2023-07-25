36401f6dd79b41b80cf94d7172a46c86a617f104
kibuchijw <kibuchijw@gmail.com>
2023-07-25 21:55:37 +0000

    Added header file and made some tweaks

---
+++ tests/implement_Cd.c
@@ -0,0 +1,28 @@
+#include "shell_K_J.h"
+
+/**
+  *implement_cd - function that changes working directory
+  *@command: user input command
+  *@args: array of strings with command arguments
+  *@arg_count: Number of elements in the array
+  *
+  *Return: void
+  */
+void implement_Cd(char *command, char **args, int arg_count)
+{
+	const char *message;
+
+	if (arg_count != 2)
+	{
+		message = "Usage: cd < current-directory>\n";
+		write(2, message, _strlen(message));
+	}
+	else
+	{
+		if (chdir(args[1]) == -1)
+		{
+			perror("cd failed");
+		}
+	}
+	(void)command;
+}
